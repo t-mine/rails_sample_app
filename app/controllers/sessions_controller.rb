dc19119725a7c006a3cbcc91387fb630729394e9
t-mine <tomonori.mine.biz@gmail.com>
2024-07-28 15:06:04 +0900

    add session create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
+  #ログイン画面を表示
   def new
+  end
+
+  #ログイン処理
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      # エラーメッセージを作成する
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
   end
 end
